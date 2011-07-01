ea2c6b9c21c937c49aea67b56cf7a274fbca283c
Mikolaj Habryn <dichro@rcpt.to>
2011-07-01 16:01:36 -0400

    better error for object resolving

--- mudryn/lib.py
+++ mudryn/lib.py
@@ -1,7 +1,21 @@
-def get_class( kls ):
+class ClassNotFoundError(AttributeError):
+  def __init__(self, target, module, attr):
+    self.target = target
+    self.module = module
+    self.attr = attr
+
+  def __str__(self):
+    return 'Subclass %s not found in module %s for %s' % (
+        self.attr, self.module, self.target)
+
+
+def get_class(kls):
   parts = kls.split('.')
   module = ".".join(parts[:-1])
   m = __import__( module )
   for comp in parts[1:]:
-    m = getattr(m, comp)
+    try:
+      m = getattr(m, comp)
+    except AttributeError:
+      raise ClassNotFoundError(kls, m.__name__, comp)
   return m
